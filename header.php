f80a6447ea6237a60c90539817ff644aa00ab41f
Knut Karlsen <knut@klapp.no>
2012-04-25 23:33:14 +0200

    Ascii-art in header, minor fixes

--- header.php
+++ header.php
@@ -1,10 +1,30 @@
 <?php include_once 'wordpress-functions.php' ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<!----------------------------------------------------------------------------------------------------------
+............................................................................................................
+................................................. made by ..................................................
+..............  __  __ _____   _______ ______ ______   _______ _______ _____ _______ _______  ..............
+.............. |  |/  |     |_|   _   |   __ \   __ \ |   |   |    ___|     \_     _|   _   | ..............
+.............. |     <|       |       |    __/    __/ |       |    ___|  --  ||   |_|       | ..............
+.............. |__|\__|_______|___|___|___|  |___|    |__|_|__|_______|_____/_______|___|___| ..............
+............................................................................................................
+............................................. http://klapp.no ..............................................
+............................................................................................................
+----------------------------------------------------------------------------------------------------------->	
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	
 <title>Olavsfestdagene 2012</title>
-	
+
+<meta name="description" content="Velkommen til de 50. Olavsfestdagene i Trondheim, 28. juli - 05. august 2012" />
+<meta name="keywords" content="olavsfestdagene,festival,konsert,arrangement,underholdning,fest,olsok,olavsvaka,nidarosdomen" />
+<meta name="author" content="Klapp Media, http://klapp.no/" />
+
+<meta name="og:title" content="Olavsfestdagene 2012" />
+<meta name="og:url" content="" />
+<meta name="og:image" content="" />
+<meta name="og:type" content="" />
+
 <link rel="stylesheet" type="text/css" media="screen" href="Assets/Css/master.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="Assets/Css/events.css" />
 <link rel="alternate" title="<?php bloginfo('name'); ?> &raquo; Feed" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" />
@@ -16,12 +36,10 @@
 <link rel="shortcut icon" href="Assets/Images/favicon.ico" />
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://use.typekit.com/mwr3aik.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-
-<script src="Assets/Js/jquery.klapp.slideshow.js"></script>
+<script src="http://use.typekit.com/mwr3aik.js"></script>
 <script src="Assets/Js/jquery.functions.js"></script>
 <script src="Assets/Js/jquery.events.functions.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
 <!--[if lt IE 9]><script src="Assets/Js/html5.js"></script><![endif]-->
 
 <?php wp_head(); ?>
